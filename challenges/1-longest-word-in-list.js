9c7ef65964f50d4762110a6bb229411c1aafecee
Salman H <Salman0112358@outlook.com>
2022-07-04 15:07:02 +0100

    final changes was to add remaining test cases

--- challenges/1-longest-word-in-list.js
+++ challenges/1-longest-word-in-list.js
@@ -22,3 +22,5 @@ function longestInList(stringArray){
 }
 
 console.log(longestInList(["England", "Wales", "Scotland", "Northern Ireland"]), "should be Northern Ireland")
+console.log(longestInList(["the", "quick", "brown", "fox"]), "should be quick")
+console.log(longestInList(["hello", "hi", "greetings", "hey"]),  "should be greetings")
