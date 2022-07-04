44d693af24610e0464a0a1bebbc184fe4e710689
Salman H <Salman0112358@outlook.com>
2022-07-04 14:33:43 +0100

    adding test cases from example file

---
+++ challenges/1-longest-word-in-list.js
@@ -0,0 +1,9 @@
+
+
+/*
+test cases:
+===========
+longestInList(["England", "Wales", "Scotland", "Northern Ireland"]) should be "Northern Ireland"
+longestInList(["the", "quick", "brown", "fox"]) should be "quick"
+longestInList(["hello", "hi", "greetings", "hey"]) should be "greetings"
+*/
