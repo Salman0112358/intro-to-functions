d2977a21e433f833c5bac6236d51309358489af5
Salman H <Salman0112358@outlook.com>
2022-07-04 14:34:29 +0100

    adding the function signature

--- challenges/1-longest-word-in-list.js
+++ challenges/1-longest-word-in-list.js
@@ -7,3 +7,7 @@ longestInList(["England", "Wales", "Scotland", "Northern Ireland"]) should be "N
 longestInList(["the", "quick", "brown", "fox"]) should be "quick"
 longestInList(["hello", "hi", "greetings", "hey"]) should be "greetings"
 */
+
+function longestInList(stringArray){
+
+}
